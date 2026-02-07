a8bacd76e6c485935e6b12f61bc86b07a1d6f9a5
David Karlsson <davidkarlssson@gmail.com>
2026-02-07 12:39:27 +0100

    Fix: disable greetd (causes VM freezes), use console autologin

--- modules/niri.nix
+++ modules/niri.nix
@@ -9,16 +9,20 @@
     package = pkgs.unstable.niri;
   };
 
-  # Enable required services for Niri
-  services.greetd = {
-    enable = true;
-    settings = {
-      default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --cmd niri-session";
-        user = "greeter";
-      };
-    };
-  };
+  # Disable greetd - causes VM freezes
+  # TODO: Re-enable once VM graphics issues are resolved
+  # services.greetd = {
+  #   enable = true;
+  #   settings = {
+  #     default_session = {
+  #       command = "${pkgs.greetd.tuigreet}/bin/tuigreet --cmd niri-session";
+  #       user = "greeter";
+  #     };
+  #   };
+  # };
+  
+  # Use console autologin instead
+  services.getty.autologinUser = "daphen";
 
   # Wayland session management
   environment.sessionVariables = {
